94e2f9e1292af433bb8d95f272704dde34ea0e96
Rafael Mota <rma7@cin.ufpe.br>
2018-12-06 17:47:09 -0300

    [#5] Teste de serviço para cenário convidar aluno com sucesso

--- server/src/spec/server.spec.ts
+++ server/src/spec/server.spec.ts
@@ -4,11 +4,14 @@ var base_url = "http://localhost:3000";
 
 describe("O servidor", () => {
 
-  let server: any = null; 
+  let server: any = null;
+  let token: any = null;
 
   beforeAll(async () => {
-    server = require('../index')
-    server = await server
+    let app = require('../index')
+    server = await app
+    await axios.post(`${base_url}/register`, { username: 'admin', password: '123456'})
+    token = (await axios.post(`${base_url}/api/login`, { username: 'admin', password: '123456'})).data.token
   });
 
   afterAll(() => {
@@ -18,6 +21,19 @@ describe("O servidor", () => {
   it('deveria iniciar', async () => {
       const res = await axios.get(base_url)
       await expect(res.data.status).toBe('ok')
+  })
+
+  it('cadastra usuários que logins ainda não cadastrados', async () => {
+    const res = (await axios.post('/users/invite', { 
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      data: { username: 'rma7' }
+    })).data
+    await expect(res).toBe({
+      status: 'ok',
+      message: "Invite succesfully sent to email rma7@cin.ufpe.br"
+    })
   })
 
 })
