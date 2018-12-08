a572c7b08de76c84fb87f6b5f95e1690ae53ae15
Lucas Barros <lbam@cin.ufpe.br>
2018-12-08 09:57:48 -0200

    [#24] Teste de classe feature de registrar alunos com sucesso

--- server/src/spec/repositories/student.spec.ts
+++ server/src/spec/repositories/student.spec.ts
@@ -1,13 +1,28 @@
+import StudentRepository from "../../repositories/studentRepository";
+import { MongoClient, Db } from "mongodb";
+import { MONGODB_URI } from "../../util/secrets";
+import { MONGODB_NAME } from "../../util/secrets";
 
-describe("A classe ListRepository", () => {
+describe("A classe studentRepository", () => {
+    let studentRepository: StudentRepository;
+    let db: Db;
 
-    beforeAll(() => {
-  
+    beforeAll(async () => {
+      let mongo: MongoClient = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
+      db = mongo.db(MONGODB_NAME);
+      studentRepository = new StudentRepository(db);  
     });
   
-    afterAll(() => {
-        
+    afterAll(async () => {
+      await db.dropCollection("students");
     });
-  
+    
+    it("deve registrar e resgatar estudantes", async () => {
+      const insert = await studentRepository.insertMany([{theHuxleyName: "Lucas Barros de Almeida Machado", login: "lbam", submissions: []}]);
+      expect(insert.insertedCount).toBe(1);
+      const get = await studentRepository.findAll({});
+      expect(get.length).toBe(1);
+    })
+
   })
   
